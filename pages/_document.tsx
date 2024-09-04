bd12ac9600777925be56e9167d93c6b290c9b4d5
Harshad Goswami <harshad.goswami@gmail.com>
2024-09-04 17:01:50 +0530

    forgot to add env

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  function gtag() {
-    //@ts-ignore
-    dataLayer.push(arguments);
-  }
-
   return (
     <Html lang="en">
-      <Head>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-3MMLLMBW40"
-        ></script>
-        <script>
-          window.dataLayer = window.dataLayer || []; gtag('js', new Date());
-          gtag('config', 'G-3MMLLMBW40');
-        </script>
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
